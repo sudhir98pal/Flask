74cc761e27dfe237433c062614e2f9c77785a28e
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-12-01 22:24:05 +0530

    db

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sudhirPal.db'
+
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable(False))
+    datCreated = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/')
